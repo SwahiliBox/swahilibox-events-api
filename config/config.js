fb51cf971803e7241acc817e1a8a11617da28b0a
Rodgers Kishagha <mkishagha@gmail.com>
2019-05-29 21:44:11 +0300

    setup app config and linting

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const joi = require('joi')
+
+const envVarsSchema = joi.object({
+  NODE_ENV: joi.string()
+    .allow(['development', 'production', 'test', 'staging'])
+    .required(),
+  PORT: joi.number().default(8080),
+})
+  .unknown()
+  .required()
+
+const { error, value: envVars } = joi.validate(process.env, envVarsSchema)
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`)
+}
+
+
+const config = {
+  env: envVars.NODE_ENV || 'development',
+  port: envVars.PORT,
+};
+
+module.exports = config;
