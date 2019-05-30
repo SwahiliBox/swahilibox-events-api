54c8effcf3965f57d7e748bd84f29ae15c03fdd6
Rodgers Kishagha <mkishagha@gmail.com>
2019-05-30 09:06:28 +0300

    remove unnecessary dependency

--- app/index.js
+++ app/index.js
@@ -1,11 +1,10 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import routes from './routes'
 
 const app = express()
 
-app.use(bodyParser.json())
+app.use(express.json())
 if (app.get('env') === 'development') {
   app.use(morgan('dev'))
 }
