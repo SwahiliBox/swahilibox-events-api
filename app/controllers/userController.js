decd205112d8adf990e44006705c2c6bca54d434
Charitie <charitytarus21@gmail.com>
2019-08-09 22:33:10 +0300

    move signup logic to user service

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,29 +1,8 @@
-import uuid from 'uuid/v4'
-import db from '../../database/models'
-import encryptPassword from '../../lib/helpers/encrypt'
+import UserService from '../services/userService'
 
 class UserController {
   static async signup(req, res) {
-    const { firstName, lastName, email, password } = req.body
-    const hashedPassword = encryptPassword.generateHash(password)
-    const userData = {
-      id: uuid(),
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-    }
-    try {
-      const user = await db.User.findOne({ where: { email } })
-
-      if (user) {
-        return res.status(409).json({ message: 'email already exists' })
-      }
-      await db.User.create(userData)
-      return res.json({ message: 'Signup successful' })
-    } catch (error) {
-      return res.status(500).json({ message: error.message })
-    }
+    return UserService.createUser(req, res)
   }
 }
 
---
+++ app/services/userService.js
@@ -0,0 +1,32 @@
+import uuid from 'uuid/v4'
+import db from '../../database/models'
+import encryptPassword from '../../lib/helpers/encrypt'
+
+class UserService {
+  static async createUser(req, res) {
+    {
+      const { firstName, lastName, email, password } = req.body
+      const hashedPassword = encryptPassword.generateHash(password)
+      const userData = {
+        id: uuid(),
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      }
+      try {
+        const user = await db.User.findOne({ where: { email } })
+
+        if (user) {
+          return res.status(409).json({ message: 'email already exists' })
+        }
+        await db.User.create(userData)
+        return res.json({ message: 'Signup successful' })
+      } catch (error) {
+        return res.status(500).json({ message: error.message })
+      }
+    }
+  }
+}
+
+export default UserService
