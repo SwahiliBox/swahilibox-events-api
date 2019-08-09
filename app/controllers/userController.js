805dc91a6554bc3d4ba6b44a8f0eed22b30a8b30
Charitie <charitytarus21@gmail.com>
2019-08-09 13:12:36 +0300

    modifying success message

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,15 +1,9 @@
 import uuid from 'uuid/v4'
 import db from '../../database/models'
-import validateSignupData from '../../lib/helpers/validateSignupData'
 import encryptPassword from '../../lib/helpers/encrypt'
 
 class UserController {
   static async signup(req, res) {
-    const errors = validateSignupData(req.body)
-
-    if (errors.length) {
-      return res.status(400).json({ message: errors[0].message })
-    }
     const { firstName, lastName, email, password } = req.body
     const hashedPassword = encryptPassword.generateHash(password)
     const userData = {
@@ -26,7 +20,7 @@ class UserController {
         return res.status(409).json({ message: 'email already exists' })
       }
       await db.User.create(userData)
-      return res.json({ message: 'a user attempted to signup to the app' })
+      return res.json({ message: 'Signup successful' })
     } catch (error) {
       return res.status(500).json({ message: error.message })
     }
