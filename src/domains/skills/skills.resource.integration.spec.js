18556f6b29eaf79dc3f07dd4a9c69015e5ddb60c
Rodgers Kishagha <mkishagha@gmail.com>
2019-12-04 13:04:08 +0300

    implement get skil by name and tag resource functions

--- src/domains/skills/skills.resource.integration.spec.js
+++ src/domains/skills/skills.resource.integration.spec.js
@@ -1,5 +1,6 @@
 import { describeDbTestSuite } from '../../test/describeDbTestSuite';
 import { skillsResource } from './skills.resource';
+import { SkillNotFoundError } from './skill.errors';
 
 describeDbTestSuite('SkillsResource', () => {
   describe('create skill', () => {
@@ -22,6 +23,56 @@ describeDbTestSuite('SkillsResource', () => {
         expect(error).not.toBe(null);
         expect(error.code).toEqual(pgUniqueViolationErrorCode);
       }
+    });
+  });
+
+  describe('getSkillByName', () => {
+    test('it should throw an error if skill not found', async () => {
+      try {
+        await skillsResource.getSkillByName('javascript');
+      } catch (error) {
+        expect(error).toBeInstanceOf(SkillNotFoundError);
+        expect(error.message).toBe('no skill with name javascript found');
+      }
+    });
+    test('it should get a skill given a valid name', async () => {
+      await skillsResource.create({ name: 'javascript' });
+      const skill = await skillsResource.getSkillByName('javascript');
+      expect(skill).not.toBe(null);
+      expect(skill.name).toBe('javascript');
+    });
+  });
+
+  describe('getSkillsByTags', () => {
+    const createSkillBody1 = {
+      name: 'ember',
+      tags: ['ember', 'ember.js', 'emberjs', 'ember js', 'javascript'],
+    };
+    const createSkillBody2 = {
+      name: 'react',
+      tags: ['react js', 'javascript'],
+    };
+
+    test('it should get only the skills with both given tags', async () => {
+      await skillsResource.create(createSkillBody1);
+      await skillsResource.create(createSkillBody2);
+      const skills = await skillsResource.getSkillsByTags([
+        'ember',
+        'javascript',
+      ]);
+      expect(skills).not.toBe(null);
+      expect(skills.length).toEqual(1);
+      expect(skills[0].name).toEqual('ember');
+    });
+
+    test('it should get all skills with the given tag', async () => {
+      await skillsResource.create(createSkillBody1);
+      await skillsResource.create(createSkillBody2);
+      const skills = await skillsResource.getSkillsByTags(['javascript']);
+      expect(skills).not.toBe(null);
+      expect(skills.length).toEqual(2);
+      expect(skills[0].name).toEqual('ember');
+      expect(skills[1].name).toEqual('react');
     });
   });
 });
--- src/domains/skills/skills.resource.js
+++ src/domains/skills/skills.resource.js
@@ -18,10 +18,43 @@ class SkillsResource {
       .where('name', skillName)
       .first();
 
-    if (!skill.length) {
-      throw SkillNotFoundError(404, `no skill with name ${skillName} found`);
+    if (!skill) {
+      throw new SkillNotFoundError(
+        404,
+        `no skill with name ${skillName} found`,
+      );
     }
     return skill;
+  }
+
+  async updateSkill(updateBody, skillId) {
+    const updatedSkill = await knexInstance(SKILLS_TABLE)
+      .update(updateBody)
+      .where('id', skillId)
+      .returning(['id', 'name', 'tags']);
+
+    return updatedSkill;
+  }
+
+  async updateSkillNameOrStatus(field, value, skillId) {
+    const updatedSkill = await knexInstance(SKILLS_TABLE)
+      .update(field, value)
+      .where('id', skillId)
+      .returning(['id', 'name', 'tags']);
+
+    return updatedSkill;
+  }
+
+  async getSkillsByTags(tags) {
+    const skills = knexInstance(SKILLS_TABLE)
+      .select('*')
+      .where(knexInstance.raw(`tags @> '{${tags}}'`));
+
+    if (!skills) {
+      throw new SkillNotFoundError(404, `no skill with tags ${tags} found`);
+    }
+
+    return skills;
   }
 }
 
